554b069f2604377c17035640693da61bbb023246
Sunnythesun <sunnyliang@live.com.au>
2022-05-23 23:59:17 +1000

    layout

--- javascript.js
+++ javascript.js
@@ -1,3 +1,52 @@
+const body = document.body;
+const calcContainer = document.createElement("div");
+const flexContainer = document.createElement("div");
+calcContainer.setAttribute("id", "calc-container");
+flexContainer.setAttribute("id", "flex-container");
+body.append(calcContainer);
+
+for (let i = 0; i < 2; i++) {
+    const div = document.createElement("div");
+    calcContainer.append(div);
+    div.style.backgroundColor = "rgb(116,179,199)";
+
+    if (i === 0) {
+        calcContainer.append(flexContainer);
+    }
+}
+
+for (let i = 0; i < 18; i++) {
+    const div = document.createElement("div");
+    flexContainer.append(div);
+    //div.style.backgroundColor = "rgb(116,179,99)";
+
+    if (i === 0) {
+        div.style.backgroundColor = "rgb(116,179,99)";
+        div.setAttribute("id", "screen-container");
+
+        for (let i = 0; i < 2; i++) {
+            const screenDiv = document.createElement("div");
+            div.append(screenDiv);
+        }
+
+    }
+    else if (i === 1) {
+        div.style.backgroundColor = "rgb(16,79,99)";
+        div.setAttribute("id", "answer-container");
+        const answerDiv = document.createElement("div");
+
+        for (let i = 0; i < 2; i++) {
+            const answerDiv = document.createElement("div");
+            div.append(answerDiv);
+        }
+    }
+}
+
+
+
+
+
+
 function add(a, b) {
     return a + b;
 }
@@ -15,7 +64,6 @@ function divide(a, b) {
 }
 
 
-//let operator = "add";
 function operate(operator, num, num2) {
 
     if (operator === "add") {
